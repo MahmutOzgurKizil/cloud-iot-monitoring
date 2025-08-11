b5b44166030bb865d16b2860eacdd7bdb1a0783c
MahmutOzgurKizil <ozgurkizil@hotmail.com>
2025-08-11 13:52:13 +0300

    Update docker-compose and test script

--- test/test_random_scores.sh
+++ test/test_random_scores.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_URL="http://127.0.0.1:5000/score"
+API_URL="http://127.0.0.1:80/score"
 COUNT=200
 
 for ((i=1; i<=COUNT; i++)); do
@@ -11,6 +11,7 @@ for ((i=1; i<=COUNT; i++)); do
 
   response=$(curl -s -X POST "$API_URL" \
     -d "player_id=$player_id&score=$score")
+
 
   echo "Response: $response"
 done
